318fbf3df35cb22cdf80de67731011efe6bc3cac
jesus morales <moralespanfilo2@gmail.com>
2018-02-04 01:47:26 -0600

    Simple java app with spring

---
+++ src/main/java/com/oreilly/RunDemo.java
@@ -0,0 +1,30 @@
+package com.oreilly;
+
+import com.oreilly.config.AppConfig;
+import com.oreilly.entities.Game;
+import com.oreilly.entities.Team;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class RunDemo {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Game game = context.getBean("game", Game.class);
+        System.out.println(game.playGame());
+        
+//        Team royals = context.getBean("royals", Team.class);
+//        Team redSox = context.getBean("redSox", Team.class);
+//        Team cubs = context.getBean("cubs", Team.class);
+//
+//        game.setHomeTeam(royals);
+//        game.setAwayTeam(cubs);
+//        game.playGame();
+//
+//        game.setHomeTeam(cubs);
+//        game.setAwayTeam(redSox);
+//        game.playGame();
+//
+        context.close();
+    }
+}
---
+++ src/main/java/com/oreilly/config/AppConfig.java
@@ -0,0 +1,52 @@
+package com.oreilly.config;
+
+import com.oreilly.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+//@ComponentScan(basePackages = "com.oreilly")
+//@EnableAspectJAutoProxy
+public class AppConfig {
+	
+	@Bean
+	public Game game() {
+		return new BaseballGame(redSox(),cubs());
+	}
+	
+	@Bean
+	public Team redSox() {
+		return new RedSox();
+	}
+	
+	@Bean
+	public Team cubs() {
+		return new RedSox();
+	}
+	
+	
+//    @Autowired
+//    private DataSource dataSource;
+
+//    @Autowired
+//    private List<Team> teams;
+//
+//    @Bean
+//    public NumberFormat nf() {
+//        return NumberFormat.getCurrencyInstance();
+//    }
+//
+//    @Bean
+//    @Scope("prototype")
+//    public Game game() {
+//        BaseballGame baseballGame = new BaseballGame(teams.get(0),teams.get(1));
+////        baseballGame.setDataSource(dataSource);
+//        return baseballGame;
+//    }
+}
